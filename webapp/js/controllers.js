87259063a2891ac117e771d69165ccbcd4443e67
msartor92 <michele01001101@gmail.com>
2016-10-06 17:15:20 +0200

    add log file

--- webapp/js/controllers.js
+++ webapp/js/controllers.js
@@ -3,13 +3,13 @@ angular.module('bike').controller('tableData', ['NetworkSvc', function(NetworkSv
         self.citySelect = -1;
        
         NetworkSvc.cityList().then(function(res){
-			self.cityList = res.data;
-		}, function(err){ console.error('cityList does not responds');});
+                self.cityList = res.data;
+        }, function(err) { console.lerr('cityList does not responds')});
 
         self.submit = function(){ 
 
-			NetworkSvc.cityData(self.citySelect).then(function(res) {
-				self.data = res.data;
-			}, function(err){ console.error('getData does not responds');});                
+                NetworkSvc.cityData(self.citySelect).then(function(res) {
+                        self.data = res.data;
+                }, function(err){ console.error('getData does not responds');});                
         };
 }]);
